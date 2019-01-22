8ae314f7291e62b11422f7e24384b022972e7bd2
Yao-Dongzhe <Yao.Dongzhe@Teradata.com>
2019-01-22 16:07:11 +0800

    增加公安人群需求

--- src/main/java/RoamingAndPartyUserForDHX.scala
+++ src/main/java/RoamingAndPartyUserForDHX.scala
@@ -17,14 +17,15 @@ object RoamingAndPartyUserForDHX extends TimeFunc with Serializable {
 
 
   var lastTime = Calendar.getInstance().getTime
+  var gonganLastTime = Calendar.getInstance().getTime
   val timeFreq: Long = 300000L
+  val gonganTimeFreq: Long = timeFreq * 12 * 3
 
   def main(args: Array[String]) {
     val sparkConfig = new SparkConfig
     val conf = sparkConfig.getConf.setAppName("RoamingAndPartyUserForDHX")
     val ssc = new StreamingContext(conf, Seconds(120))
     val hun = new HbaseUtilNew
-    val hunBro = ssc.sparkContext.broadcast(hun)
 
 
     val topicSet = Array(Set("O_DPI_LTE_S1_MME"), Set("O_DPI_MC_LOCATIONUPDATE_2G"), Set("O_DPI_MC_LOCATIONUPDATE_3G"))
@@ -36,31 +37,47 @@ object RoamingAndPartyUserForDHX extends TimeFunc with Serializable {
     )
 
 
-    def getPartyUsersFromHbase: List[String] = {
-      var listResult = List[String]()
+    def getPartyUsersFromHbase: Set[String] = {
+      var ResultSet = Set[String]()
       hun.getAllRows("DHXSelectPhoneList").foreach(r => {
-        listResult = Bytes.toString(r.getRow) +: listResult
+        ResultSet = ResultSet.+(Bytes.toString(r.getRow))
       })
-      listResult
+      ResultSet
+    }
+
+    def getGonganCustFromHdfs: Set[String] = {
+      val gonganPath = "hdfs://nmdsj133nds/user/caodongwei/pdata_floatingnet_sc_phonelist/*"
+      ssc.sparkContext.textFile(gonganPath).collect().toSet
     }
 
 
-    val lacCiActIdListBro = BroadcastWrapper[List[String]](ssc, getPartyUsersFromHbase)
+    val lacCiActIdSetBro = BroadcastWrapper[Set[String]](ssc, getPartyUsersFromHbase)
+    val gonganCustSetBro = BroadcastWrapper[Set[String]](ssc, getGonganCustFromHdfs)
 
     def updateBroadcast() {
 
 
-      //每隔1分钟更新广播变量
+      //每隔5分钟更新广播变量
       val currTime = toDate
       val diffTime = currTime.getTime - lastTime.getTime
       if (diffTime > timeFreq) {
 
-        lacCiActIdListBro.update(getPartyUsersFromHbase, blocking = true)
-
+        lacCiActIdSetBro.update(getPartyUsersFromHbase, blocking = true)
         lastTime = toDate
       }
     }
 
+    def updateGonganBroadcast() {
+
+      //每隔3小时更新广播变量
+      val currTime = toDate
+      val diffTime = currTime.getTime - gonganLastTime.getTime
+      if (diffTime > gonganTimeFreq) {
+
+        gonganCustSetBro.update(getGonganCustFromHdfs, blocking = true)
+        gonganLastTime = toDate
+      }
+    }
 
     val numStreams = 3
     val kafkaStreams = (1 to numStreams).map { i => KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicSet(i - 1)) }
@@ -134,144 +151,145 @@ object RoamingAndPartyUserForDHX extends TimeFunc with Serializable {
     filterStream.foreachRDD(rdd => {
 
       updateBroadcast
+      updateGonganBroadcast
 
-      if (lacCiActIdListBro.value.size > 0) {
-        rdd.partitionBy(new HashPartitioner(partitions = 100)).foreachPartition(partition => {
-          //          kafka-topics.sh --zookeeper zk-01:2181,zk-02:2181,zk-03:2181/kafka --create --replication-factor 1 --partitions 7 --topic ROAMING-AND-PARTY-USER-FOR-DHX
-          val targetTopic = "ZNDX-USER-FOR-DHX"
-          val area = new AreaList
-          val xinganmengArea = new xinganmengAreaList
-          val fengzhenArea = new fengzhenAreaList
-          val xinganmengWuchakouArea = new XinganmengWuchakouArea
-          val ganqimaoduAreaList = new GanqimaoduAreaList
-          val xinghexianAreaList = new XinghexianAreaList
-          val chenbaerhuqiAreaList = new ChenbaerhuqiAreaList
-          val eerduosiAreaList = new EerduosiAreaList
+      rdd.partitionBy(new HashPartitioner(partitions = 100)).foreachPartition(partition => {
+        //          kafka-topics.sh --zookeeper zk-01:2181,zk-02:2181,zk-03:2181/kafka --create --replication-factor 1 --partitions 7 --topic ROAMING-AND-PARTY-USER-FOR-DHX
+        val gonganCustSet = gonganCustSetBro.value
+        val lacCiActIdSet = lacCiActIdSetBro.value
 
-          //          val hunValue = hunBro.value
-          //          val conn = hunValue.createHbaseConnection
-          val aershanLacCiList = area.aershanLacCiList
-          val xinBaerhuzuoqi = area.xinBaerhuzuoqi
-          val erlianhaote = area.erlianhaote
-          val wulagai = area.wulagai
-          val alashan = area.alashan
-          val elunchun = area.elunchun
-          val ejinaqi = area.ejinaqi
-          val wulanchabu = area.wulanchabu
-          val baotouyidong = area.baotouyidong
-          val manzhouli = area.manzhouli
-          val siziwangqi = area.siziwangqi
-          val keerqinyouyizhongqi = area.keerqinyouyizhongqi
-          val xinganmengnongmuyeju1 = xinganmengArea.xinganmengnongmuyeju1
-          val xinganmengnongmuyeju2 = xinganmengArea.xinganmengnongmuyeju2
-          val xinganmengnongmuyeju3 = xinganmengArea.xinganmengnongmuyeju3
-          val wengniute = area.wengniute
-          val dengkousanshenggong = area.dengkousanshenggong
-          val fengzheng = fengzhenArea.fengzheng
-          val tuoxian = area.tuoxian
-          val xinganmengWuchakou = xinganmengWuchakouArea.xinganmengWuchakouSet
-          val ganqimaodu = ganqimaoduAreaList.ganqimaoduAreaSet
-          val wulatehouqi = area.wulatehouqi
-          val xinghexian = xinghexianAreaList.xinghexian
-          val chenbaerhuqi = chenbaerhuqiAreaList.chenbaerhuqi
-          val eerduosi = eerduosiAreaList.eerduosi
-          val eerduosiLeaders = eerduosiAreaList.eerduosiLeaders
+        val targetTopic = "ZNDX-USER-FOR-DHX"
+        val area = new AreaList
+        val xinganmengArea = new xinganmengAreaList
+        val fengzhenArea = new fengzhenAreaList
+        val xinganmengWuchakouArea = new XinganmengWuchakouArea
+        val ganqimaoduAreaList = new GanqimaoduAreaList
+        val xinghexianAreaList = new XinghexianAreaList
+        val chenbaerhuqiAreaList = new ChenbaerhuqiAreaList
+        val eerduosiAreaList = new EerduosiAreaList
+        val aershanLacCiList = area.aershanLacCiList
+        val xinBaerhuzuoqi = area.xinBaerhuzuoqi
+        val erlianhaote = area.erlianhaote
+        val wulagai = area.wulagai
+        val alashan = area.alashan
+        val elunchun = area.elunchun
+        val ejinaqi = area.ejinaqi
+        val wulanchabu = area.wulanchabu
+        val baotouyidong = area.baotouyidong
+        val manzhouli = area.manzhouli
+        val siziwangqi = area.siziwangqi
+        val keerqinyouyizhongqi = area.keerqinyouyizhongqi
+        val xinganmengnongmuyeju1 = xinganmengArea.xinganmengnongmuyeju1
+        val xinganmengnongmuyeju2 = xinganmengArea.xinganmengnongmuyeju2
+        val xinganmengnongmuyeju3 = xinganmengArea.xinganmengnongmuyeju3
+        val wengniute = area.wengniute
+        val dengkousanshenggong = area.dengkousanshenggong
+        val fengzheng = fengzhenArea.fengzheng
+        val tuoxian = area.tuoxian
+        val xinganmengWuchakou = xinganmengWuchakouArea.xinganmengWuchakouSet
+        val ganqimaodu = ganqimaoduAreaList.ganqimaoduAreaSet
+        val wulatehouqi = area.wulatehouqi
+        val xinghexian = xinghexianAreaList.xinghexian
+        val chenbaerhuqi = chenbaerhuqiAreaList.chenbaerhuqi
+        val eerduosi = eerduosiAreaList.eerduosi
+        val eerduosiLeaders = eerduosiAreaList.eerduosiLeaders
 
-          partition
-            .toList
-            .sortBy(_._2._1._2)
-            .foreach(kline => {
+        partition
+          .toList
+          .sortBy(_._2._1._2)
+          .foreach(kline => {
 
-              val line = kline._2
-              val lacCiActIdList = lacCiActIdListBro.value
+            val line = kline._2
 
-              val phone_no = line._2
-              val local_city = line._3
-              val roam_type = line._4
-              val owner_province = line._5
-              val owner_city = line._6
-              val lac = line._7
-              val ci = line._8
-              val lac_ci = lac + "-" + ci
-              val startTime: String = line._1._1
-              val startTimeLong: Long = line._1._2
-
-
-              val stringLine = startTime + "|" +
-                phone_no + "|" +
-                local_city + "|" +
-                roam_type + "|" +
-                owner_province + "|" +
-                owner_city + "|" +
-                lac + "|" +
-                ci
+            val phone_no = line._2
+            val local_city = line._3
+            val roam_type = line._4
+            val owner_province = line._5
+            val owner_city = line._6
+            val lac = line._7
+            val ci = line._8
+            val lac_ci = lac + "-" + ci
+            val startTime: String = line._1._1
+            val startTimeLong: Long = line._1._2
 
 
-              def send(eventType: Int): Unit = {
-                kafkaProducer.value.send(targetTopic, eventType.toString + "|" + stringLine)
+            val stringLine = startTime + "|" +
+              phone_no + "|" +
+              local_city + "|" +
+              roam_type + "|" +
+              owner_province + "|" +
+              owner_city + "|" +
+              lac + "|" +
+              ci
+
+
+            def send(eventType: Int): Unit = {
+              kafkaProducer.value.send(targetTopic, eventType.toString + "|" + stringLine)
+            }
+            //            党建人群1
+            if (lacCiActIdSet.contains(phone_no)) send(1)
+            //            公安人群32
+            if (gonganCustSet.contains(phone_no)) send(32)
+            //              兴安盟农牧业局21
+            if (roam_type.equals("4") && (xinganmengnongmuyeju1.contains(lac_ci)
+              || xinganmengnongmuyeju2.contains(lac_ci)
+              || xinganmengnongmuyeju3.contains(lac_ci)
+              )
+            ) send(21)
+            //                  乌拉特后旗30
+            if (local_city.equals("0478") && wulatehouqi.contains(lac_ci)) send(30)
+            //                兴安盟五岔沟28
+            if (xinganmengWuchakou.contains(lac_ci)) send(28)
+            //            满洲里18
+            if (manzhouli.contains(lac_ci)) send(18)
+            //              甘其毛都29
+            if (ganqimaodu.contains(lac_ci)) send(29)
+            //              包头移动17
+            else if (baotouyidong.contains(lac_ci)) send(17)
+            //              四子王旗19
+            else if (siziwangqi.contains(lac_ci)) send(19)
+            //            阿尔山5
+            //            else if (local_city.equals("0482") && aershanLacCiList.contains(lac_ci)) {
+            else if (local_city.equals("0482")) send(5)
+            //              漫入人群
+            else {
+              if (!roam_type.equals("4") && !roam_type.equals("")) {
+                //                呼和浩特27
+                if (local_city.equals("0471") && !owner_city.equals("0471")) send(27)
+                //                翁牛特旗22
+                if (local_city.equals("0476") && wengniute.contains(lac_ci)) send(22)
+                //                乌兰察布，丰镇26
+                if (local_city.equals("0474") && fengzheng.contains(lac_ci)) send(26)
+                //                磴口三盛公景区24
+                if (local_city.equals("0478") && dengkousanshenggong.contains(lac_ci)) send(24)
+                //            漫入赤峰人群(所在地市，漫游类型)id:12
+                else if (local_city.equals("0476")) send(12)
+                //            新巴尔虎左旗6
+                else if (local_city.equals("0470") && xinBaerhuzuoqi.contains(lac_ci)) send(6)
+                //            陈巴尔虎旗31
+                else if (local_city.equals("0470") && chenbaerhuqi.contains(lac_ci)) send(31)
+                //            二连浩特3
+                else if (local_city.equals("0479") && erlianhaote.contains(lac_ci)) send(3)
+                //            锡林郭勒乌拉盖4
+                else if (local_city.equals("0479") && wulagai.contains(lac_ci)) send(4)
+                //              阿拉善右旗8
+                else if (local_city.equals("0483") && alashan.contains(lac_ci)) send(8)
+                //              乌兰察布10
+                else if (local_city.equals("0474") && wulanchabu.contains(lac_ci)) send(10)
+                //              阿拉善额济纳旗13
+                else if (local_city.equals("0483") && ejinaqi.contains(lac_ci)) send(13)
+                //                  科尔沁右翼中旗20
+                else if (local_city.equals("0482") && keerqinyouyizhongqi.contains(lac_ci)) send(20)
+                //                  呼和浩特，托县25
+                else if (local_city.equals("0471") && tuoxian.contains(lac_ci)) send(25)
+                //                鄂尔多斯7
+                else if (local_city.equals("0477") && eerduosi.contains(lac_ci)) send(7)
               }
-              //            党建人群1
-              if (lacCiActIdList.contains(phone_no)) send(1)
-              //              兴安盟农牧业局21
-              if (roam_type.equals("4") && (xinganmengnongmuyeju1.contains(lac_ci)
-                || xinganmengnongmuyeju2.contains(lac_ci)
-                || xinganmengnongmuyeju3.contains(lac_ci)
-                )
-              ) send(21)
-              //                  乌拉特后旗30
-              if (local_city.equals("0478") && wulatehouqi.contains(lac_ci)) send(30)
-              //                兴安盟五岔沟28
-              if (xinganmengWuchakou.contains(lac_ci)) send(28)
-              //            满洲里18
-              if (manzhouli.contains(lac_ci)) send(18)
-              //              甘其毛都29
-              if (ganqimaodu.contains(lac_ci)) send(29)
-              //              包头移动17
-              else if (baotouyidong.contains(lac_ci)) send(17)
-              //              四子王旗19
-              else if (siziwangqi.contains(lac_ci)) send(19)
-              //            阿尔山5
-              //            else if (local_city.equals("0482") && aershanLacCiList.contains(lac_ci)) {
-              else if (local_city.equals("0482")) send(5)
-              //              漫入人群
-              else {
-                if (!roam_type.equals("4") && !roam_type.equals("")) {
-                  //                呼和浩特27
-                  if (local_city.equals("0471") && !owner_city.equals("0471")) send(27)
-                  //                翁牛特旗22
-                  if (local_city.equals("0476") && wengniute.contains(lac_ci)) send(22)
-                  //                乌兰察布，丰镇26
-                  if (local_city.equals("0474") && fengzheng.contains(lac_ci)) send(26)
-                  //                磴口三盛公景区24
-                  if (local_city.equals("0478") && dengkousanshenggong.contains(lac_ci)) send(24)
-                  //            漫入赤峰人群(所在地市，漫游类型)id:12
-                  else if (local_city.equals("0476")) send(12)
-                  //            新巴尔虎左旗6
-                  else if (local_city.equals("0470") && xinBaerhuzuoqi.contains(lac_ci)) send(6)
-                  //            陈巴尔虎旗31
-                  else if (local_city.equals("0470") && chenbaerhuqi.contains(lac_ci)) send(31)
-                  //            二连浩特3
-                  else if (local_city.equals("0479") && erlianhaote.contains(lac_ci)) send(3)
-                  //            锡林郭勒乌拉盖4
-                  else if (local_city.equals("0479") && wulagai.contains(lac_ci)) send(4)
-                  //              阿拉善右旗8
-                  else if (local_city.equals("0483") && alashan.contains(lac_ci)) send(8)
-                  //              乌兰察布10
-                  else if (local_city.equals("0474") && wulanchabu.contains(lac_ci)) send(10)
-                  //              阿拉善额济纳旗13
-                  else if (local_city.equals("0483") && ejinaqi.contains(lac_ci)) send(13)
-                  //                  科尔沁右翼中旗20
-                  else if (local_city.equals("0482") && keerqinyouyizhongqi.contains(lac_ci)) send(20)
-                  //                  呼和浩特，托县25
-                  else if (local_city.equals("0471") && tuoxian.contains(lac_ci)) send(25)
-                  //                鄂尔多斯7
-                  else if (local_city.equals("0477") && eerduosi.contains(lac_ci)) send(7)
-                }
-              }
-            })
-        }
-        )
+            }
+          })
       }
+      )
+
     })
 
 
